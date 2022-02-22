a44d53857e2ed346b20a3334a115c16954669d8f
Brijesh <dobariyabrijesh07@gmail.com>
2022-02-22 10:53:25 +0530

    app.js changes

--- week-02/src/App.js
+++ week-02/src/App.js
@@ -1,11 +1,12 @@
 // import SampleApp from "./day-01/01-sample-app/SampleApp";
-import LoginForm from "./day-01/02-login-form/LoginForm";
+// import LoginForm from "./day-01/02-login-form/LoginForm";
 import "./App.css";
+import ClassComponent from "./day-02/ClassComponent";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <ClassComponent />
     </div>
   );
 }
