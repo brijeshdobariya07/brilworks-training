feee4797fe24a2d77992f32505fdae0412739ffa
Brijesh <dobariyabrijesh07@gmail.com>
2022-02-22 11:50:53 +0530

    day-02 training

--- week-02/src/App.js
+++ week-02/src/App.js
@@ -1,12 +1,12 @@
 // import SampleApp from "./day-01/01-sample-app/SampleApp";
 // import LoginForm from "./day-01/02-login-form/LoginForm";
-import "./App.css";
 import ClassComponent from "./day-02/ClassComponent";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ClassComponent />
+      <ClassComponent name="Brijesh " />
     </div>
   );
 }
--- week-02/src/day-02/ClassComponent.js
+++ week-02/src/day-02/ClassComponent.js
@@ -3,13 +3,14 @@ import React, { Component } from "react";
 class ClassComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { name: props.name };
   }
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        <h2>Props Name : {this.props.name}</h2>
+        <h2>State Name : {this.state.name}</h2>
       </div>
     );
   }
