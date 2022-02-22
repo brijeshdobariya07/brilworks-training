400937cd76df0547315d97055522c61e72063d97
Brijesh <dobariyabrijesh07@gmail.com>
2022-02-22 12:12:30 +0530

    task1 completed

--- day-05/task-01/script.js
+++ day-05/task-01/script.js
@@ -1,15 +1,28 @@
-function main() {
-  var last_Name = "Dobariya";
-  function lastName() {
-    console.log(last_Name);
-    return last_Name;
-  }
-  function firstName() {
-    console.log("Brijesh");
-    setTimeout(() => {
-      lastName();
-    }, 5000);
-  }
-  firstName();
+function firstName() {
+  var first_name = "Brijesh";
+  document.getElementById("fname").textContent = first_name;
+  console.log(first_name);
 }
+
+function lastName(lname) {
+  console.log(lname);
+}
+
+async function main() {
+  var last_name = "Dobariya";
+  firstName();
+
+  // we wait till this execution finish so we used await to pause
+  await new Promise((res, rej) => {
+    setTimeout(() => {
+      res(lastName(last_name));
+    }, 5000);
+  });
+
+  // after it finish we can continue rest of the process
+  setTimeout((res, rej) => {
+    document.getElementById("lname").textContent = last_name;
+  }, 5000);
+}
+
 main();
